d726b2f718d19019dca026030706d57b8bbe1610
suap <suap.coding@gmail.com>
2021-06-22 10:17:03 +0200

    Some stuff.

---
+++ first-file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Hello guys!")
+
+
+# Another thingy.
+
+# Guy was here
+
+# Rats branch
+
+# In[ ]:
+
+
+import numpy as np
+y = np.random.randint(-10,10,size=(10,10,10))
+y
+
+
+# In[ ]:
+
+
+for x in y:
+    for z in x:
+        print(z)
+
