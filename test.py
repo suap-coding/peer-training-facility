5af2ae3a03a8298eaf9a680c9bfbc3622aee6857
suap <suap.coding@gmail.com>
2021-06-27 21:07:26 +0200

    Super fun thingy I did add yes.

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import numpy as np
 
+arr = np.random.randint(1,10, size=[3,3,3])
+
+for a in arr:
+    for b in a:
+        for c in b:
+            print(c,end=' ')
+        print()
+    print()
