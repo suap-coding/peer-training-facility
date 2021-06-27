cbe196c821eb212925784fb170dace5023455520
Murtaza <murtazajits@gmail.com>
2021-06-27 21:11:42 +0200

    just tested the merge

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import numpy as np
 
 arr = np.random.randint(1,10, size=[3,3,3])
 
-a = np.array [1,2,3,4,5]
+a = np.array ([1,2,3,4,5])
 print (a)
 
 
